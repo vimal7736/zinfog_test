982c08802bdb338c9fd4c25c9e27a492028e3294
Vimal Suresh <109647667+vimal7736@users.noreply.github.com>
2023-10-10 16:04:03 +0530

    login updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardPage from './components/DashboardPage';
 import ResetPasswordPage from './auth/ResetPasswordPage';
 import LoginPage from './auth/LoginPage';
@@ -8,28 +8,22 @@ import ChangePasswordPage from './auth/ChangePasswordPage';
 import OTPConfirmationPage from './auth/OtpConfirmation';
 
 const App = () => {
-  
-  const isLoggedIn = false; 
-
   return (
     <Router>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<LoginPage/>} />
         <Route path="reset-password" element={<ResetPasswordPage />} />
         <Route path="reset-password-success-page" element={<PasswordResetSuccessPage />} />
         <Route path="change-password" element={<ChangePasswordPage />} />
-        <Route path="enter-otp" element={<OTPConfirmationPage />} />
+        <Route path="enter-otp" element={<OTPConfirmationPage/>} />
 
-       
-        <Route
-          path="/dashboard/*"
-          element={isLoggedIn ? <DashboardPage /> : <Navigate to="/login" />}
-        />
-
-      
         <Route
           path="/"
-          element={isLoggedIn ? <Navigate to="/dashboard" /> : <Navigate to="/login" />}
+          element={
+              <Routes>
+                <Route index element={<DashboardPage />} />
+              </Routes>
+          }
         />
       </Routes>
     </Router>
